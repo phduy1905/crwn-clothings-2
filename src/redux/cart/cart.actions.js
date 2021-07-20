840be61c27b3e26fc92ba5b19982aa6ecfa93c29
phduy1905 <phduy1905@gmail.com>
2021-07-20 12:47:50 +0700

    add addItem and removeItem action to CheckOutItem Component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItem = (item) => ({
   type: CartActiontypes.ADD_ITEM,
   payload: item,
 });
+
+export const removeItem = (item) => ({
+  type: CartActiontypes.REMOVE_ITEM,
+  payload: item,
+});
+
+export const clearItemFromCart = (item) => ({
+  type: CartActiontypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
